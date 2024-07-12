2cf61850b7dd266fff2df8fd38baf4a2d3fe91a2
KadirovKmck <kadirovmuhammadaziz79@gmail.com>
2024-07-12 17:12:11 +0600

    k

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:t89/src/data/datasources/remote/remote_data_source.dart.dart';
-import 'package:t89/src/data/repositories/authentication_repository_impl.dart';
-import 'package:t89/src/domain/repositories/authentication_repository.dart';
-import 'package:t89/src/domain/usecases/get_user.dart';
-import 'package:t89/src/presentation/routes/app_routes.dart';
-
-final sl = GetIt.instance;
-
-void setupLocator() {
-  // Register BLoCs
-  sl.registerFactory(() => AuthenticationBloc(sl()));
-
-  // Register use cases
-  sl.registerLazySingleton(() => GetUser(sl()));
-
-  // Register repositories
-  sl.registerLazySingleton<AuthenticationRepository>(
-      () => AuthenticationRepositoryImpl(remoteDataSource: sl()));
-
-  // Register data sources
-  sl.registerLazySingleton<RemoteDataSource>(
-      () => RemoteDataSourceImpl(client: sl()));
-
-  // Register external dependencies
-  sl.registerLazySingleton(() => http.Client());
-}
+import 'package:t89/src/presentation/pages/settings_screen.dart';
 
 void main() {
-  setupLocator();
   runApp(const MyApp());
 }
 
@@ -36,8 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      onGenerateRoute: AppRoutes.generateRoute,
+    return MaterialApp(
+      home: SettingsScreen(),
+      // onGenerateRoute: AppRoutes.generateRoute,
     );
   }
 }
--- lib/src/presentation/routes/app_routes.dart
+++ lib/src/presentation/routes/app_routes.dart
@@ -1,13 +1,12 @@
-import 'package:flutter/material.dart';
-import 'package:t89/src/presentation/pages/home_page.dart';
+// import 'package:flutter/material.dart';
 
-class AppRoutes {
-  static Route<dynamic> generateRoute(RouteSettings settings) {
-    switch (settings.name) {
-      case '/':
-        return MaterialPageRoute(builder: (_) => HomePage());
-      default:
-        return MaterialPageRoute(builder: (_) => HomePage());
-    }
-  }
-}
+// class AppRoutes {
+//   static Route<dynamic> generateRoute(RouteSettings settings) {
+//     switch (settings.name) {
+//       case '/':
+//         return MaterialPageRoute(builder: (_) => HomePage());
+//       default:
+//         return MaterialPageRoute(builder: (_) => HomePage());
+//     }
+//   }
+// }
